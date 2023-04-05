74c22f24926fbda9a392ff71a19cf830e2d96d68
Ript0rr <Antoinecraft0@gmail.com>
2023-04-05 15:58:17 +0200

    quand on clique sur le cube il nous suit

--- Assets/MoveTo.cs
+++ Assets/MoveTo.cs
@@ -2,25 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class MoveTo : MonoBehaviour
 {
-
+    public bool activated;
     public Transform goal;
-    
-    // Start is called before the first frame update
+    public NavMeshAgent agent;
+   
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
 
-        Vector3 vc = (goal.position - transform.position);
-        agent.destination = transform.position + vc.normalized * (vc.magnitude - 3f);
+        //Quand on fait clique gauche
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            //Lancer un rayon dans la direction de la caméra
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hit;
 
+            //Action du rayon
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.gameObject == this.gameObject)
+                {
+                    activated = true;
+
+                }
+            }
+        }
+        if (activated)
+        {
+            Vector3 vc = (goal.position - transform.position);
+            agent.destination = transform.position + vc.normalized * (vc.magnitude - 3f);
+        }
     }
+
 }
